8eebf63787427d1cbd3d8522d1a323e4642dc6ee
Karl Jackson <karljax@gmail.com>
2013-04-04 10:29:34 -0500

    node and socket demo

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
-var socket = require('socket.io');
-var express = require('express');
-
-var app = express();
-var io = socket.listen(app);
-
-app.get('/', function(req, resp) {
-  resp.sendfile('index.html');
-});
-
-app.get('socket.io/socket.io.js', function(req, res) {
-  res.sendfile('node_modules/socket.io/lib/socket.io.js');
-});
-
-io.sockets.on('connection', function(client) {
-  console.log("a client has connected");
-});
+var app = require('http').createServer(handler)
+  , io = require('socket.io').listen(app)
+  , fs = require('fs')
 
 app.listen(8080);
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('giveMeData', function (data) {
+    console.log('heres your data');
+  });
+});
