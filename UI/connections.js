29b02af5c90706f7263f27d4b7985f40b65b1224
Paul <pstahmer25@gmail.com>
2013-04-09 12:19:28 -0500

    More or less same thing

---
+++ UI/connections.js
@@ -0,0 +1,34 @@
+ //socket functions for communication and event handling from the server
+ //PRD
+	var socketConnectionURL = "http://boiling-meadow-5174.herokuapp.com/";
+	if (window.location.toString().indexOf("localhost")>=0)
+	{
+		//localhost
+		socketConnectionURL ="http://localhost:8080";
+	}
+ var socket = io.connect(socketConnectionURL);
+ 
+ socket.on('connect', function (data) {
+	//alert('client is connected');
+  });
+  
+  socket.on('playerListUpdate', function (data) {
+	//alert('playerListUpdate');
+	drawNames(data);
+  });
+  
+  socket.on('finalBoardUpdate', function (data) {
+	//debugDiv.innerHTML = JSON.stringify(data);
+	receiveData(data);
+	//renderBoard(data); // this isnt working, not sure why it doesnt draw the board correctly
+	//receiveData(JSON.stringify(data)); // nope doesnt work either
+	
+  });
+  
+  socket.on('partialBoardUpdate', function (data) {
+	writeCounter++;
+	myVar=setTimeout(function(){
+		//writeDebug(data);
+		receiveData(data);
+	},writeCounter*1000);
+  });
