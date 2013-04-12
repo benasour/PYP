74ad22e22b88b67576dd7d7d47c56753aae1e4c9
Paul <pstahmer25@gmail.com>
2013-04-12 10:40:01 -0500

    trying to fix

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ app.get('/games', function(req, res) {
   //TODO: Get list of games from mongo DB
   var gamesList = new Array();
   gamesList.push({name: "Horses"});
+  gamesList.push({name: "Card Horses"});
   gamesList.push({name: "Another Drinking Game To Implement"});
 
   res.render('games', {games: gamesList});
@@ -63,8 +64,8 @@ io.sockets.on('connection', function (socket) {
   var toSend = {};
   if (started) // if a game is in progress, tell this person!
   {
-	  toSend["status"] = "started";
-	  socket.emit('sendStatus', toSend);
+      toSend["status"] = "started";
+      socket.emit('sendStatus', toSend);
   }
   toSend = {};
   toSend["players"] = players;
@@ -73,96 +74,87 @@ io.sockets.on('connection', function (socket) {
   socket.broadcast.emit('playerListUpdate', toSend);
   
   socket.on('requestPlayers', function(){
-	  var toSend = {};
-	  toSend["players"] = players;
-	  console.log(toSend);
-	  socket.emit('playerListUpdate', toSend);
-	  socket.broadcast.emit('playerListUpdate', toSend);
+      var toSend = {};
+      toSend["players"] = players;
+      console.log(toSend);
+      socket.emit('playerListUpdate', toSend);
+      socket.broadcast.emit('playerListUpdate', toSend);
   });
   
   socket.on('join', function (name, bet) {
     console.log('I am the Join function!');
-<<<<<<< HEAD
-	players[name]=bet;
-	//wrap in another layer to format it as a type for interactions.js
-	var toSend = {};
-	toSend["players"] = players;
-	console.log(toSend);
-	socket.emit('playerListUpdate', toSend);
-	socket.broadcast.emit('playerListUpdate', toSend);
-=======
-	  players[name]=0;
-	  //wrap in another layer to format it as a type for interactions.js
-	  var toSend = {};
-	  toSend["players"] = players;
-	  console.log(toSend);
-	  socket.emit('playerListUpdate', toSend);
-	  socket.broadcast.emit('playerListUpdate', toSend);
->>>>>>> master
+
+    players[name]=bet;
+    //wrap in another layer to format it as a type for interactions.js
+    var toSend = {};
+    toSend["players"] = players;
+    console.log(toSend);
+    socket.emit('playerListUpdate', toSend);
+    socket.broadcast.emit('playerListUpdate', toSend);
   });
   
   socket.on('new', function () {
     console.log('I am the new game function!');
-	  started = false;
-	  //reset the players!
-	  players = {};
-	
-	  //tell other clients that a new game is starting
-	  var toSend = {};
-	  toSend["status"] = "new";	
-	  socket.emit('playerListUpdate', toSend);
-	  socket.broadcast.emit('playerListUpdate', toSend);
-	
-	  //reset everyone's player lists
-	  toSend = {};
-	  toSend["players"] = players;
-	  console.log(toSend);
-	  socket.emit('playerListUpdate', toSend);
-	  socket.broadcast.emit('playerListUpdate', toSend);
+      started = false;
+      //reset the players!
+      players = {};
+    
+      //tell other clients that a new game is starting
+      var toSend = {};
+      toSend["status"] = "new";    
+      socket.emit('playerListUpdate', toSend);
+      socket.broadcast.emit('playerListUpdate', toSend);
+    
+      //reset everyone's player lists
+      toSend = {};
+      toSend["players"] = players;
+      console.log(toSend);
+      socket.emit('playerListUpdate', toSend);
+      socket.broadcast.emit('playerListUpdate', toSend);
   });
   
   socket.on('startGame', function () {
-	  started = true;
+      started = true;
     console.log('I am the Start Game function!');
-	  var status = {};
-	  status["status"] = "start";
-	  console.log(status);
-	  socket.broadcast.emit('sendStatus', status);	
-	
-	  var trackLength = 8; //from index.html
-	
-	  //this is where we loop
-	  var data = {};
-	  //um, this number thing is wierd - make it true/false
-	  var finishLine=0;
-	  finished = false;
-	  while (!finished)
-	  {
+      var status = {};
+      status["status"] = "start";
+      console.log(status);
+      socket.broadcast.emit('sendStatus', status);    
+    
+      var trackLength = 8; //from index.html
+    
+      //this is where we loop
+      var data = {};
+      //um, this number thing is wierd - make it true/false
+      var finishLine=0;
+      finished = false;
+      while (!finished)
+      {
       finishLine++;
-		  for (var i = 0; i<Object.keys(players).length; i++)
-			  data[Object.keys(players)[i]] = players[Object.keys(players)[i]];
-		  var rnd = Math.floor(Math.random()*Object.keys(players).length);
-		  players[Object.keys(data)[rnd]]++;
-		  //receiveData(JSON.stringify(data));
-		
-		  for (var j = 0; j<Object.keys(players).length; j++)
-		  {
+          for (var i = 0; i<Object.keys(players).length; i++)
+              data[Object.keys(players)[i]] = players[Object.keys(players)[i]];
+          var rnd = Math.floor(Math.random()*Object.keys(players).length);
+          players[Object.keys(data)[rnd]]++;
+          //receiveData(JSON.stringify(data));
+        
+          for (var j = 0; j<Object.keys(players).length; j++)
+          {
         if (players[Object.keys(players)[j]] >= trackLength)
-			  {
-				  finished=true;
-			  }
-		  }
-		  var toSend = {};
-		  toSend["players"] = players;
-	
-		  console.log(toSend);
-		  console.log(finishLine);
-		  socket.emit('partialBoardUpdate', toSend);
-		  socket.broadcast.emit('partialBoardUpdate', toSend);	
-	  }
-	  //send the final result to UI for updating
-	  //socket.emit('finalBoardUpdate', players2);
-	  //socket.broadcast.emit('finalBoardUpdate', players2);
+              {
+                  finished=true;
+              }
+          }
+          var toSend = {};
+          toSend["players"] = players;
+    
+          console.log(toSend);
+          console.log(finishLine);
+          socket.emit('partialBoardUpdate', toSend);
+          socket.broadcast.emit('partialBoardUpdate', toSend);    
+      }
+      //send the final result to UI for updating
+      //socket.emit('finalBoardUpdate', players2);
+      //socket.broadcast.emit('finalBoardUpdate', players2);
   });
   
 });
