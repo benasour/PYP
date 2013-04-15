e198029e69d8740b5c7bdc979d9b713ef9649ed8
benasour <benasour7@gmail.com>
2013-04-15 16:42:48 -0500

    re-adding mongo db backend

--- app.js
+++ app.js
@@ -3,17 +3,19 @@ var http = require('http');
 var socket = require('socket.io');
 var express = require('express');
 var jade = require('jade');
+var mongoose = require('mongoose');
 
 var app = express();
 var server = http.createServer(app);
 var io = socket.listen(server);
-var started = false;
-var clients = 0;
-var clientsWaiting = 0;
 
 //heroku suggestion to use port 8080
 var port = process.env.PORT || 8080;
 server.listen(port);
+
+//Models and db connection
+var Game = require('./Models/game');
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/PYP');
 
 //configuration settings for express and socket.io
 //production environment only
@@ -38,13 +40,11 @@ app.get('/', function(req, res) {
 });
 
 app.get('/games', function(req, res) {
-  //TODO: Get list of games from mongo DB
-  var gamesList = new Array();
-  gamesList.push({name: "Horses"});
-  gamesList.push({name: "Card Horses"});
-  gamesList.push({name: "Another Drinking Game To Implement"});
-
-  res.render('games', {games: gamesList});
+  Game.find({}, function(err, games) {
+    if (!err) {
+      res.render('games', {games: games});
+    }
+  });
 });
 
 //look into using express.static
@@ -61,7 +61,9 @@ app.get('/interactions.js', function(req, res) {
 });
 
 var players = new Array();
-
+var started = false;
+var clients = 0;
+var clientsWaiting = 0;
 io.sockets.on('connection', function (socket) {
   console.log('A new connection has been created');
   var toSend = {};
